f6e6b056bcbe927455981cd7a0587d33beb24644
lang <itlaolang@163.com>
2018-02-24 12:20:21 +0800

    12

--- src/main/java/test/test.java
+++ src/main/java/test/test.java
@@ -1,8 +1,17 @@
+/**
+ * 
+ */
 package test;
 
+/**
+ * @author itlaolang
+ * @date 2018年2月24日 
+ * @version：v 1.0 
+ * @Description：github第一次测试
+ */
 public class test {
 	public static void main(String[] args) {
-		System.out.println("github测试");
+		System.out.println("测试");
 	}
 
 }
